7787da6b46ad83723b92e11b6922f8af6071e855
Joao Antunes <antunes.joao150@gmail.com>
2021-10-15 11:47:14 -0600

    Modified init file, so that now we create a tire model based on the class

--- __init__.py
+++ __init__.py
@@ -2,16 +2,3 @@ from . import src
 from .src.TireModel import PAC2002, Pacejka94
 from .src.Core.tirestate import TireState
 
-def tire(model_name):
-    if model_name == 'Harty':
-        print('Harty not implemented')
-        return None
-
-    elif model_name == 'PAC2002':
-        return PAC2002()
-
-    elif model_name == 'Pacejka94':
-        return Pacejka94()
-
-    else:
-        return None  # Of we cam
